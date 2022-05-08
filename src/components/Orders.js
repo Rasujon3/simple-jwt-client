f258eae78eba5a09cfaa58198a80a3e6030c4b7c
Rasujon3 <rasujon3@gmail.com>
2022-05-08 23:14:17 +0600

    70_5-5 Verify Token and handle unauthorized token

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Orders = () => {
+  const navigate = useNavigate();
   const [orders, setOrders] = useState([]);
   useEffect(() => {
     const url = `http://localhost:5000/orders`;
@@ -10,14 +12,20 @@ const Orders = () => {
         authorization: `Bearer ${localStorage.getItem("accessToken")}`,
       },
     })
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.status === 401 || res.status === 403) {
+          navigate("/login");
+        }
+        return res.json();
+      })
       .then((data) => {
         console.log(data);
+        setOrders(data);
       });
   }, []);
   return (
     <div>
-      <h1>This is Orders page</h1>
+      <h1>This is Orders page: {orders.length}</h1>
     </div>
   );
 };
