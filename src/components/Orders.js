8b34bc932d9e019633b42a2c8b151d2b8cc0f3d5
Rasujon3 <rasujon3@gmail.com>
2022-05-08 23:01:51 +0600

    70_5-4 Send JWT Token to the server side and set middletier

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const Orders = () => {
-    return (
-        <div>
-            <h1>This is Orders page</h1>
-        </div>
-    );
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    const url = `http://localhost:5000/orders`;
+    fetch(url, {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>This is Orders page</h1>
+    </div>
+  );
 };
 
 export default Orders;
